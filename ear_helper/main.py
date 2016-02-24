89fc8231af174bb9b9b791c703a51faa11e06daa
natanfrost <nataniel.r.v@gmail.com>
2016-02-24 18:01:20 -0300

    modified:   ear_helper/main.py modified:   ear_helper/main.pyc modified:   ear_helper/player.py

--- ear_helper/main.py
+++ ear_helper/main.py
@@ -1,76 +1,11 @@
-import pygame
-from pygame import mixer, display, event
+from pygame import mixer
 from os import path
-from time import sleep
 from random import choice
-import sys
 
 notes_folder = path.dirname(path.abspath(__file__)) + '/notes/{note}.mp3'
 current_note = ''
 
-def play_note(note):
-    running = True
+def play_note(self):
     mixer.init()
-    mixer.music.load(notes_folder.format(note=note))
-    while running:
-        mixer.music.play()
-        sleep(3)
-        running = False
-
-
-times = int(raw_input('How many times: '))
-acertos = 0
-vezes = 0
-
-display.set_mode((300, 100))
-display.set_caption('text')
-font = pygame.font.SysFont('Arial', 36)
-text = font.render("Hello There", 1, (10, 10, 10))
-textpos = text.get_rect()
-textpos.centerx = background.get_rect().centerx
-background.blit(text, textpos)
-
-while times != 0:
-    random_note = choice('abcdefg')
-    play_note(random_note)
-    continua = True
-    while continua:
-        for event in pygame.event.get():
-
-            if event.type == pygame.QUIT:
-                sys.exit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    note = 'a'
-                    continua = False
-                elif event.key == pygame.K_b:
-                    note = 'b'
-                    continua = False
-                elif event.key == pygame.K_c:
-                    note = 'c'
-                    continua = False
-                elif event.key == pygame.K_d:
-                    note = 'd'
-                    continua = False
-                elif event.key == pygame.K_e:
-                    note = 'e'
-                    continua = False
-                elif event.key == pygame.K_f:
-                    note = 'f'
-                    continua = False
-                elif event.key == pygame.K_g:
-                    note = 'g'
-                    continua = False
-                elif event.key == pygame.K_SPACE:
-                    play_note(random_note)
-
-    if note == random_note:
-        acertos = acertos + 1
-    else:
-        print random_note
-    times = times - 1
-    vezes = vezes + 1
-
-porcentagem = (100 * acertos) / vezes
-
-#print 'Acertou %i de %i\n%d%% de aproveitamento.' % (acertos, vezes, porcentagem)
+    mixer.music.load(notes_folder.format(note='a'))
+    mixer.music.play()
--- ear_helper/player.py
+++ ear_helper/player.py
@@ -8,9 +8,7 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-import pygame
-from pygame import mixer, display, event
-from os import path
+import main
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -27,6 +25,24 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_Form(object):
+    def __init__(self):
+      super(Ui_Form, self).__init__()
+
+    def notify(self, receiver, event):
+      if(event.type() == QEvent.KeyPress):
+	QMessageBox.information(None,"Received Key Press Event!!",
+				     "You Pressed: "+ event.text())
+      #Call Base Class Method to Continue Normal Event Processing
+      return super(MyApplication, self).notify(receiver, event)
+
+    def keyPressEvent(self, event):
+         if type(event) == QtGui.QKeyEvent:
+             #here accept the event and do something
+             print event.key()
+             event.accept()
+         else:
+             event.ignore()
+
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
         Form.resize(400, 300)
@@ -45,14 +61,7 @@ class Ui_Form(object):
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Form", None))
         self.btnPlayChord.setText(_translate("Form", "Play chord", None))
-        self.btnPlayChord.clicked.connect(self.play_note)
-
-    def play_note(self):
-        notes_folder = path.dirname(path.abspath(__file__)) + '/notes/{note}.mp3'
-        mixer.init()
-        mixer.music.load(notes_folder.format(note='a'))
-        mixer.music.play()
-
+        self.btnPlayChord.clicked.connect(main.play_note)
 
 if __name__ == "__main__":
     import sys
